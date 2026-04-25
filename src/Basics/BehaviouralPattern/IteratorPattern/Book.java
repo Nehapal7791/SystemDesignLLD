02f4e0ad56925506ddf0ef82bb01f55ea91b6ec8
Neha Pal <palneha671@gmail.com>
2026-04-25 22:49:08 +0530

    iterator design pattern

---
+++ src/Basics/BehaviouralPattern/IteratorPattern/Book.java
@@ -0,0 +1,27 @@
+package Basics.BehaviouralPattern.IteratorPattern;
+
+public class Book implements Comparable<Book>{
+    private String title;
+    Book(String title)
+    {
+        this.title=title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return this.title.compareTo(o.title);
+    }
+
+
+}
