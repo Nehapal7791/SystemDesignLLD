72cf1bc5f3dd6e3e4e4fa90ef8c6a6e7115e5a7d
Neha Pal <palneha671@gmail.com>
2026-05-01 22:19:55 +0530

    builder pattern

---
+++ src/Basics/CreationPattern/BuilderPattern/with/WithBuilderPattern.java
@@ -0,0 +1,13 @@
+package Basics.CreationPattern.BuilderPattern.with;
+
+public class WithBuilderPattern {
+    public static void main(String[] args) {
+        House house = new House.HouseBuilder("Concrete", "Wooden", "Tile")
+                .setGarden(true)
+                .setSwimmingPool(true)
+                .setGarage(false)
+                .build();
+
+        System.out.println(house);
+    }
+}
---
+++ src/Basics/CreationPattern/BuilderPattern/without/House.java
@@ -0,0 +1,44 @@
+package Basics.CreationPattern.BuilderPattern.without;
+
+
+public class House {
+    private String foundation;
+    private String structure;
+    private String roof;
+    private boolean hasGarage;
+    private boolean hasSwimmingPool;
+    private boolean hasGarden;
+
+    //Constructor
+    public House(String foundation,String structure,String roof, boolean hasGarage,boolean hasSwimmingPool,boolean hasGarden){
+        this.foundation = foundation;
+        this.structure = structure;
+        this.roof = roof;
+        this.hasGarage = hasGarage;
+        this.hasSwimmingPool = false;
+        this.hasGarden = hasGarden;
+    }
+
+    @Override
+    public String toString() {
+        return "House{" +
+                "foundation='" + foundation + '\'' +
+                ", structure='" + structure + '\'' +
+                ", roof='" + roof + '\'' +
+                ", hasGarage=" + hasGarage +
+                ", hasSwimmingPool=" + hasSwimmingPool +
+                ", hasGarden=" + hasGarden +
+                '}';
+    }
+
+    public static class WithoutBuilderPattern {
+        public static void main(String[] args) {
+            House house = new House("Concrete","Wood","Shingles",true,true,false);
+            //House house2 = new House("Concrete","Wood","Shingles");
+            // Constructor Explosion -> Too Many Constructors
+            // Difficult to understand and maintain this code
+            // this is where builder pattern comes into picture
+            System.out.println(house);
+        }
+    }
+}
